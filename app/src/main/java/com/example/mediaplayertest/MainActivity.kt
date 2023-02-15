bf389ce04e3b86f780be3f3ae98b7a46e84aecd1
wuyuetao <wuyuetao@bilibili.com>
2023-02-15 17:51:33 +0800

    新增：通过prepare初始化，添加暂停/继续，布局修改

--- app/src/main/java/com/example/mediaplayertest/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayertest/MainActivity.kt
@@ -18,35 +18,33 @@ import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    private var mService = MusicService()
-
-    private val mServiceConntection = object : ServiceConnection {
-        override fun onServiceConnected(compName: ComponentName?, binder: IBinder?) {
-            mService = (binder as MyBinder).service
-        }
-
-        override fun onServiceDisconnected(p0: ComponentName?) {
-            mService = MusicService()
-        }
-    }
-
-    fun bindServiceConnection() {
-        val intent = Intent(this, MusicService::class.java)
-        startService(intent)
-        bindService(intent, mServiceConntection, BIND_AUTO_CREATE)
-    }
+    private var mPlayer = MediaPlayer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn: Button = findViewById(R.id.button)
-        btn.setOnClickListener {
+        val createBtn: Button = findViewById(R.id.create_btn)
+        createBtn.setOnClickListener {
             doCreate()
         }
 
-        val text: TextView = findViewById(R.id.play_status)
+        val prepareBtn: Button = findViewById(R.id.prepare_btn)
+        prepareBtn.setOnClickListener {
+            doPrepare()
+        }
 
+        val pauseBtn: Button = findViewById(R.id.pause_btn)
+        pauseBtn.setOnClickListener {
+            doPause()
+        }
+
+        val resumeBtn: Button = findViewById(R.id.resume_btn)
+        resumeBtn.setOnClickListener {
+            doResume()
+        }
+
+        val text: TextView = findViewById(R.id.play_status)
         val getStatusBtn: Button = findViewById(R.id.status)
         getStatusBtn.setOnClickListener {
             text.text = MediaPlayerUtil.getStatus()
@@ -54,16 +52,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun doCreate() {
+        mPlayer = MediaPlayer()
         mPlayer = MediaPlayer.create(this, R.raw.testcut)
         mPlayer.start()
     }
 
-    private var mPlayer = MediaPlayer()
+    private fun doPrepare() {
+        mPlayer = MediaPlayer()
+        val uri = Uri.parse("android.resource://com.example.mediaplayertest/" + R.raw.testcut)
+        try {
+            mPlayer.setDataSource(this, uri)
+            mPlayer.prepareAsync()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        mPlayer.setOnPreparedListener{
+            mPlayer.start()
+        }
+    }
 
-    private val listener= MediaPlayer.OnPreparedListener { mPlayer.start() }
+    private fun doPause() {
+        mPlayer.pause()
+    }
+
+    private fun doResume() {
+        mPlayer.start()
+    }
+
+    private val listener = MediaPlayer.OnPreparedListener { mPlayer.start() }
 
     private fun doMediainit() {
-        val uri = Uri.parse("android.resource://com.android.sim/"+R.raw.testcut)
+        val uri = Uri.parse("android.resource://com.android.sim/" + R.raw.testcut)
         mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
         try {
             mPlayer.setDataSource(this, uri)
@@ -95,7 +114,7 @@ class MainActivity : AppCompatActivity() {
         })
         val url = Uri.parse("//music.163.com/outchain/player?type=2&id=2006471884&auto=1&height=66")
         val file = File(Environment.getExternalStorageDirectory(), "iwish.mp3")
-        val uri = Uri.parse("android.resource://com.android.sim/"+R.raw.testcut)
+        val uri = Uri.parse("android.resource://com.android.sim/" + R.raw.testcut)
         try {
             MediaPlayerUtil.prepare(uri, this)
         } catch (e: Exception) {
@@ -111,5 +130,23 @@ class MainActivity : AppCompatActivity() {
         } else {
 
         }
+    }
+
+    private var mService = MusicService()
+
+    private val mServiceConntection = object : ServiceConnection {
+        override fun onServiceConnected(compName: ComponentName?, binder: IBinder?) {
+            mService = (binder as MyBinder).service
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            mService = MusicService()
+        }
+    }
+
+    fun bindServiceConnection() {
+        val intent = Intent(this, MusicService::class.java)
+        startService(intent)
+        bindService(intent, mServiceConntection, BIND_AUTO_CREATE)
     }
 }
